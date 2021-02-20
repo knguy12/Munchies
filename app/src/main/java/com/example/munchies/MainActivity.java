032bf8b33a99ac1a0045c20316da4da8a1095de1
Kevin Nguyen <nguyenkevin7326@gmail.com>
2021-02-19 18:01:50 -0800

    Added in Splash Screen

--- app/src/main/java/com/example/munchies/MainActivity.java
+++ app/src/main/java/com/example/munchies/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.munchies;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    private static int SPASH_TIME_OUT = 4000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPASH_TIME_OUT);
     }
 }
